a70c9f0930bafbda9d543e271bea08067b82b402
EdmundoJ <edmundojsjunior@gmail.com>
2025-07-03 15:41:41 -0300

    Retirando id desnecessário na classe itemRequest

--- Shopping.API/Domain/Models/Request/ItemRequest.cs
+++ Shopping.API/Domain/Models/Request/ItemRequest.cs
@@ -2,14 +2,11 @@
 {
     public class ItemRequest
     {
-       
-        public int IdItem { get; set; } 
         public int IdProd { get; set; }
         public int QuantidadeProd { get; set; }
 
         public ItemRequest(int item, int idProd, int quantidadeProd)
         {
-            IdItem = item;
             IdProd = idProd;
             QuantidadeProd = quantidadeProd;
         }
